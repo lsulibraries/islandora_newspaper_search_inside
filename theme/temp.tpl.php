e44cec1d90aa831fd9d9ac17c35baf346a2d71da
lalithchandu <lchandu1@lsu.edu>
2018-11-02 14:11:57 -0500

    Working on template removed the results form which is not required

--- theme/temp.tpl.php
+++ theme/temp.tpl.php
@@ -1,35 +1,33 @@
 <?php
 //comments...
 ?>
-
-<div class='isandora_solr_search_results'>
-    <div class='islandora_solr_search_result_inner'>
-      <?php print "You have searched for the string"?> <?php print $variables['search']?> <?php echo "<br>" ?>
-      <?php print $variables['n']?> <?php print "results found in OCR Datastream"?>
-    </div>
-</div>
-<?php foreach ($pids as $pid) : ?>
-  <div class='isandora_solr_search_results'>
-    <div class = 'islandora-solr-search-result clear-block even'>
-      <div class='islandora_solr_search_result_inner'>
-        <?php $path = $variables[$pid]['path']?>
-        <?php $label = $variables[$pid]['label']?>
-        <?php $ocr_view = $variables[$pid]['ocr_view']?>
-        <?php $tn_link = $variables[$pid]['tn_link']?>
-        <?php print "<a href= '$path' > $label</a>" ?> <?php echo "&nbsp&nbsp&nbsp&nbsp" ?>
-        <?php print "<a href='$ocr_view'>View OCR</a>" ?> <?php echo "<br>" ?>
-        <dl calss = "solr-thumb">
+<div class = "islandora-solr-content">
+  <div class = "islandora_solr_search_results">
+    <?php echo "<br>" ?><?php print "You have searched for the string"?> <?php print $variables['search']?><?php echo "<br>" ?>
+    <?php print $variables['n']?> <?php print "results found in OCR Datastream"?><?php echo "<br>" ?>
+    <?php foreach ($pids as $pid) : ?>
+    <?php $path = $variables[$pid]['path']?>
+    <?php $label = $variables[$pid]['label']?>
+    <?php $ocr_view = $variables[$pid]['ocr_view']?>
+    <?php $tn_link = $variables[$pid]['tn_link']?>
+    <div class = "islandora-solr-search-result clear-block odd">
+      <div class = "islandora-solr-search-result-inner">
+        <dl class = "solr-thumb">
           <dt>
-          <?php print "<a href='$path'><img src='$tn_link' alt='$label'></img></a>" ?>
-        </dt>
+            <?php print "<a href='$path'><img src='$tn_link' alt='$label'></img></a>"?>
+          </dt>
         </dl>
-          <dl class = 'solr-fields islandora-inline-metadata'>
-            <dt class="solr-label dc-title"><?php $label ?> </dt>
-            <dd class="solr-value dc-title">
-              <?php print "<a href= '$path'>$label</a>" ?>
+        <dl class = "solr-fields islandora-inline-metadata">
+          <dt class="solr-label dc-title">
+            <?php $label?>
+          </dt>
+          <dd class="solr-value dc-title">
+            <?php print "<a href= '$path'>$label</a>"?><?php echo "&nbsp&nbsp&nbsp&nbsp&nbsp"?>
+            <?php print "<a href='$ocr_view'>View OCR</a>"?>
           </dd>
         </dl>
       </div>
     </div>
+    <?php endforeach; ?>
   </div>
-<?php endforeach; ?>
+</div>
