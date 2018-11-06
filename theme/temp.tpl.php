28fde8c5f3a6b6b355fbccd1e6161d32947b040c
lalithchandu <lchandu1@lsu.edu>
2018-11-06 15:20:08 -0600

    wip

--- theme/temp.tpl.php
+++ theme/temp.tpl.php
@@ -5,29 +5,27 @@
   <?php $row_result = 0; ?>
   <?php print "You have searched for the string "?><?php print $variables['search']?><?php echo ",&nbsp" ?>
   <?php print $variables['n']?><?php print " results found in the OCR Datastream"?><?php echo "<br><br>"?>
-  <?php foreach ($pids as $pid) : ?>
-  <?php $path = $variables[$pid]['path']?>
-  <?php $label = $variables[$pid]['label']?>
-  <?php $ocr_view = $variables[$pid]['ocr_view']?>
-  <?php $tn_link = $variables[$pid]['tn_link']?>
-  <div class="islandora-solr-search-result clear-block <?php print $row_result % 2 == 0 ? 'odd' : 'even'; ?>">
-    <div class = "islandora-solr-search-result-inner">
-      <dl class = "solr-thumb">
-        <dt>
-          <?php print "<a href='$path'><img src='$tn_link' alt='$label'></img></a>"?>
-        </dt>
-      </dl>
-      <dl class = "solr-fields islandora-inline-metadata">
-        <dt class="solr-label dc-title">
-          <?php print $label;?>
-        </dt>
-        <dd class="solr-value dc-title">
-          <?php print "<a href= '$path'>$label</a>"?><?php print "------>"?>
-          <?php print "<a href='$ocr_view'>View OCR</a>"?>
-        </dd>
-      </dl>
+  <?php foreach ($issues as $issue) : ?>
+    <?php $path = $issue['path']?>
+    <?php $label = $issue['label']?>
+    <?php $tn_link = $issue['tn_link']?>
+    <div class="islandora-solr-search-result clear-block <?php print $row_result % 2 == 0 ? 'odd' : 'even'; ?>">
+      <div class = "islandora-solr-search-result-inner">
+        <dl class = "solr-thumb">
+          <dt>
+            <?php print "<a href='$path'><img src='$tn_link' alt='$label'></img></a>"?>
+          </dt>
+        </dl>
+        <dl class = "solr-fields islandora-inline-metadata">
+          <dt class="solr-label dc-title">
+            <?php print $label;?>
+          </dt>
+          <dd class="solr-value dc-title">
+            <?php print "<a href= '$path'>$label</a>"?><?php print "------>"?>
+          </dd>
+        </dl>
+      </div>
     </div>
-  </div>
-  <?php $row_result++; ?>
+    <?php $row_result++; ?>
   <?php endforeach; ?>
 </div>
